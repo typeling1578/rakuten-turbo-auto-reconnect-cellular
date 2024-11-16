0ddb836a42f8fa8da08a559c1f4e5fd2a8e5a1a3
typeling1578 <pub@typeling1578.dev>
2024-11-16 22:42:03 +0900

    Initial files

---
+++ dashboard-controller.mjs
@@ -0,0 +1,59 @@
+import sleep from "./sleep.mjs";
+import config from "./config.json" with { type: "json" };
+
+export async function login(page, username, password) {
+  await page.goto(`http://${config.dashboard_host}/pages.html#/login`);
+  await page.reload();
+
+  await page.waitForSelector("#activation-content-right input");
+  await page.type('#activation-content-right input[type="text"]', username);
+  await page.type('#activation-content-right input[type="password"]', password);
+  await page.click('#activation-content-right input[type="button"]');
+
+  let wait_count = 0;
+  while ((new URL(page.url())).hash != "#/overview") {
+    if (wait_count >= 10) {
+      throw new Error("Login failed");
+    }
+    await sleep(500);
+    wait_count++;
+  }
+}
+
+export async function logout(page) {
+  await page.goto(`http://${config.dashboard_host}/pages.html#/overview`);
+  await page.reload();
+
+  await page.waitForSelector(".logout_text");
+  await page.click(".logout_text");
+
+  let wait_count = 0;
+  while ((new URL(page.url())).hash != "#/login") {
+    if (wait_count >= 10) {
+      throw new Error("Logout failed");
+    }
+    await sleep(500);
+    wait_count++;
+  }
+}
+
+export async function reconfig_lte_5g(page) {
+  await page.goto(`http://${config.dashboard_host}/pages.html#/internet/lte_advanced`);
+  await page.reload();
+
+  await page.waitForSelector(".lte-advanced-content select > option");
+  const current_value = await page.$eval(".lte-advanced-content select", node => node.value);
+  await sleep(500);
+  if (current_value === "1") {
+    await page.select(".lte-advanced-content select", "2");
+  } else if (current_value === "2") {
+    await page.select(".lte-advanced-content select", "1");
+  } else {
+    throw new Error("Invalid selected option");
+  }
+  await sleep(500);
+  await page.select(".lte-advanced-content select", current_value);
+  await page.click(".lte-advanced-content .button-apply");
+
+  await sleep(10000);
+}
---
+++ index.mjs
@@ -0,0 +1,21 @@
+import puppeteer from "puppeteer";
+import { login, reconfig_lte_5g, logout } from "./dashboard-controller.mjs";
+import config from "./config.json" with { type: "json" };
+
+const browser = await puppeteer.launch({
+  headless: false,
+  executablePath: config.chromium_browser_path,
+  defaultViewport: {
+    width: 1280,
+    height: 720,
+  }
+});
+
+const page = await browser.newPage();
+
+await login(page, config.username, config.password);
+await reconfig_lte_5g(page);
+await logout(page);
+
+await page.close();
+await browser.close();
